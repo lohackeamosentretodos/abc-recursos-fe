c501f1fd8138016f298d706b7b2b48d011872f9e
Matias Camusso <mcamusso@simtlix.com>
2020-03-24 18:38:13 -0300

    fix states & add banner in request

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { States } from '../../utils/states';
+import { ISelect } from '../../utils/interfaces';
 
 @Component({
   selector: 'app-donate',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonateComponent implements OnInit {
 
+  states: ISelect [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.states = States.map((state, index) => ({
+      id: index,
+      value: state,
+    }));
   }
 
+  getSelectedState(value: string) {
+    console.log('state', value)
+  }
 }
